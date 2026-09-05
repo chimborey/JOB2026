7608d3262ddca61ca6413a5d887acd466a7cf23d
chim borey <oceansdtol@gmail.com>
2026-09-06 01:35:25 +0700

    Update

--- src/Page/Page/LoginForm.jsx
+++ src/Page/Page/LoginForm.jsx
@@ -1,10 +1,12 @@
 import LoginAccount from "../../API/Auth/LoginAccount";
+import RegisterAccount from "../../API/Auth/RegisterAccount";
 
 export default  function LoginForm(){
       return(
             <section className=" w-full overflow-hidden py-12 md:py-20">
                   <main className=" mt-12 container mx-auto md:max-w-7xl sm:px-4 md:px-6 lg:px-8">
                         <LoginAccount />
+                        <RegisterAccount />
                   </main>
             </section>
       )
